c79bba7a1fab8271ec38fea8b6338dd43be87caf
nameless-one <i.a.konev@gmail.com>
2014-10-09 12:21:43 +0400

    Added sessions

--- lib/1.php
+++ lib/1.php
@@ -1,11 +1,14 @@
 <?php
-
+//includes оставлены на хост
 class TestPub {
 	
 	private $data='';
 	public $points;
+	//кэшируется средствами любого акселератора или store в духе redis/memcache, чтобы избежать json_parse
 	private $arr;
 	public $speed;
+	private $mc;
+	private $redis;
 	private $debug=true;
 
 
@@ -40,6 +43,15 @@ class TestPub {
 
 		$this->points=sizeof($this->arr["tr"]);
 		$this->speed=$this->arr["speed"];
+
+		//основной shm xcache, не требует инициализации
+/*		$this->mc = new Memcache();
+		$this->mc->connect("localhost");
+
+		$this->redis = new Redis();
+		$this->redis->connect("localhost");
+
+*/
 	}
 	
 	function getDistance() {
@@ -59,31 +71,71 @@ class TestPub {
 	}
 
 	function getPartDistance(int $n){
-
-		return 0;
+		if ((n>$this->points)||(n<2)) {return 0;} else {
+		$distance=0;
+		for ($c=0;$c<($n-2);$c++) {
+			$distance+=$this->GetDist($this->arr["tr"][$c],$this->arr["tr"][$c+1]);
+		}
+		if ($this->debug) {echo("getPartDistance call, return $distance, point $n\n");}
+		return $distance;
+		}
 	}
 
 	function getPartTimeArrival(DateTime $dt, int $n) {
-
-		return 0;
+		$dtout=$dt;
+		$dtout->modify("+".intval($this->getPartDistance($n)/$this->speed*3600)." seconds");
+		if ($this->debug) {echo("getPartTimeArrival call, delta ".intval($this->getDistance()/$this->speed*3600).", point $n, speed $this->speed\n");}
+		return $dtout;
 	}
 
 	function play() {
-
+		//функция возвращает id сессии которая стартовала, ну и не забывая о том, что тут тоже можно даже данные маршрута сразу запихать в кэш, только опять некрасиво будет, придётся каждый раз при создании класса и вводные повторять
+		$id=uniqid("TestClass_",true);
+		//основной shm xcache
+		xcache_set("$id_start",new DateTime(),($this->getDistance()/$this->speed*3600));
+		//$this->mc->set("$id_start",new DateTime(),0,$this->getDistance()/$this->speed*3600));
+		//$this->redis->setex("$id_start",$this->getDistance()/$this->speed*3600),new DateTime());
+		return $id;
 	}
 
-	function getPlace() {
-        	
+	function getPlace($id) {
+	//тут неясно что возвращать, но вернём всё в массиве [lat,lon,%tonextpoint], оптимизировать функции в духе disttonextpoint можно очень сильно, но код будет уже некрасивый
+		if (xcache_isset($id)) {
+//		if ($this->mc->get($id)!=false) {
+//		if ($this->redis->exists($id)) {
+		$dt=xcache_get($id);
+//		$dt=$this->mc->get($id);
+//		$dt=$this->redis->get($id);
+		$dt2=new DateTime();
+		//не очень корректно на будущее т.к. unix_timestamp, но в новых php оно уже достаточного размера
+		$delta=$dt2->getTimestamp()-$dt->getTimestamp();
+		$dist=delta/3600*$this->speed;
+		$flag=2;
+		$distance=0;
+		$prevdist=0;
+			while (flag<$this->points) {
+				$distance=$this->getPartDistance($flag);
+				if $distance>=$dist {
+					$n=$flag;
+					$percent=($distance-$prevdist)/($this->speed*($delta-$prevtime))*100;
+					break 1;
+				} else {$prevdist=$distance;$prevtime=$prevdist/$this->speed*3600;}
+			}
+        	} else {return 0;}
+		$result[]=$this->arr["tr"][$n];
+		$result[]=$percent;
+		return $result;
 	}
 
 
 }
 	
 //предполагается - скорость км/ч, координаты широты/долготы в градусах.
-$test= new TestPub('{"tr": [[ 33, 33 ], [ 37, 24 ], [ 43, 45 ]], "speed": "400" }');
 
-print_r(new DateTime());
-print_r($test->getTimeArrival(new DateTime()));
+//$test= new TestPub('{"tr": [[ 33, 33 ], [ 37, 24 ], [ 43, 45 ]], "speed": "400" }');
+
+//print_r(new DateTime());
+//print_r($test->getTimeArrival(new DateTime()));
 
 
 ?>
